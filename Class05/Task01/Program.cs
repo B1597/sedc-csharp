93d2ebdebb112c490d33de59bcedf3854fb5d987
Bojana Dimovikj <bojana.dimovik@live.com>
2020-03-28 03:25:24 +0100

    On branch master

---
+++ Class05/Task01/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Task01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime currentDate = DateTime.Now;
+
+            //3 DAYS FROM NOW
+            DateTime threeDaysFromNow = currentDate.AddDays(3);
+            Console.WriteLine(threeDaysFromNow);
+
+            // 1 MONTH FROM NOW
+            DateTime monthFromNow = currentDate.AddMonths(1);
+            Console.WriteLine(monthFromNow);
+
+            //MONTH AND 3 DAYS FROM NOW
+            DateTime monthAndThreeDays = monthFromNow.AddDays(3);
+            Console.WriteLine(monthAndThreeDays);
+
+            //1 YEAR AND 2 MONTHS AGO FROM TODAY
+            DateTime twoMonthsAgo = currentDate.AddMonths(-2);
+            DateTime yearAgo = twoMonthsAgo.AddYears(-1);
+            Console.WriteLine(yearAgo);
+
+            //CURRENT MONTH
+            int currentMonth = currentDate.Month;
+            string monthName = currentDate.ToString("MMMM");
+
+            Console.WriteLine(currentMonth);
+            Console.WriteLine(monthName);
+
+            //CURRENT YEAR
+            int currentYear = currentDate.Year;
+            Console.WriteLine(currentYear);         
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Class05/Task02/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace Task02
+{
+    public class Dog
+    {
+        public string Name { get; set; }
+        public string Race { get; set; }
+        public string Color { get; set; }
+
+        public string Eat()
+        {
+            string eating = $"{Name} the {Race} is now eating.";
+            return eating;
+        }
+
+        public string Play()
+        {
+            string playing = $"{Name} the {Race} is now playing.";
+            return playing;
+        }
+
+        public string ChaseTail()
+        {
+            string chaseTail = $"{Name} the {Race} is now chasing his tail.";
+            return chaseTail;
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog doggo = new Dog();
+
+            Console.Write("Write dog's name: ");
+            doggo.Name = Console.ReadLine();
+
+            Console.Write("Write dog's race: ");
+            doggo.Race = Console.ReadLine();
+
+            Console.Write("Write dog's color: ");
+            doggo.Color = Console.ReadLine();
+
+
+            Console.Clear();
+            Console.WriteLine("Choose one of the actions");
+            Console.WriteLine("1) Eating");
+            Console.WriteLine("2) Playing");
+            Console.WriteLine("3) Chasing tail");
+            string input = Console.ReadLine();
+
+            if (input == "1")
+            {
+                Console.WriteLine(doggo.Eat());
+            }
+            else if (input == "2")
+            {
+                Console.WriteLine(doggo.Play());
+            }
+            else if (input == "3")
+            {
+                Console.WriteLine(doggo.ChaseTail());
+            }
+            else
+            {
+                Console.WriteLine("Invalid input");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
